39264ef9a26e07efd87c277577f7132372ed5621
Matthew Gruman <i@gruman.co>
2021-03-08 13:30:40 -0800

    Cleaned up
    Project cleaned up for tutorial

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -4,7 +4,9 @@ public class PlayerController : MonoBehaviour
 {
 
     private Rigidbody2D player;
-    public float speed = 3.0f;
+
+    public float speed = 6.0f;
+    private bool isIce;
 
     void Start()
     {
@@ -13,7 +15,31 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-        Vector2 direction = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")) * speed;
-        player.AddForce(direction, ForceMode2D.Force);
+        if (isIce)
+        {
+            Vector2 direction = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")) * speed;
+            player.AddForce(direction, ForceMode2D.Force);
+        }
+        else
+        {
+            player.velocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")) * speed;
+        }
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Ice")
+        {
+            isIce = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Ice")
+        {
+            isIce = false;
+        }
     }
 }
